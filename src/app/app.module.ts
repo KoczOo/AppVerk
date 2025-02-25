35e89095dc3ad8c67f2ac8f4f9c42fb4745ca784
Sebastian <sebastian.koczotowski@symmetry.pl>
2025-02-25 16:18:05 +0100

    Init (layout,fonts) + pierwszy mock template formularza logowoania.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './components/login/login.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: false,
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class LayoutComponent {
+
+}
