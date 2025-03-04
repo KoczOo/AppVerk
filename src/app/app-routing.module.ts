c9df2356783d851309f311e8afd9c9bf812ee13d
Sebastian Koczotowski <sebastian.koczotowski@gmail.com>
2025-03-04 09:40:32 +0100

    Refactor: Implementation of lazy loading for login module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LayoutComponent} from "./layout/layout.component";
-import {LoginComponent} from "./components/login/login.component";
 import {HomeComponent} from "./pages/home/home.component";
 import {authenticationGuard} from "./guards/authentication.guard";
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-    canActivate: [authenticationGuard]
-  },
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '**',
-        component: HomeComponent,
-        canActivate: [authenticationGuard]
-      }
-    ]
-  }
+    {
+        path: 'login',
+        loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule),
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '**',
+                component: HomeComponent,
+                canActivate: [authenticationGuard]
+            }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
